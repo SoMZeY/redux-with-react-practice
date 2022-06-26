f87d90255c9ae0955e88634cdf477442263f4f74
SoMZeY <vvbordiaw@gmail.com>
2022-06-25 22:44:30 -0400

    redux changed with redux toolkit

--- src/store /index.js
+++ src/store /index.js
@@ -1,27 +1,29 @@
-import { createStore } from "redux";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
 
-const countReducer = (state = initialState, action) => {
-  if (action.type === "increment") {
-    return {
-      counter: state.counter + action.amount,
-      showCounter: state.showCounter,
-    };
-  } else if (action.type === "decrement") {
-    return {
-      counter: state.counter - 1,
-      showCounter: state.showCounter,
-    };
-  } else if (action.type === "toggle") {
-    return {
-      counter: state.counter,
-      showCounter: !state.showCounter,
-    };
-  }
-  return state;
-};
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    toggleCounter() {},
+  },
+});
 
-const store = createStore(countReducer);
+const store = configureStore({
+  reducer: {
+    reducer: counterSlice.reducer,
+  },
+});
 
+export const counterActions = counterSlice.actions;
 export default store;
